d9e7aadef31c13d0644b945f51857caf577b7b53
Michal Mankiewicz <michal.mankiewicz@itechart-group.com>
2023-06-30 23:25:03 +0200

    feat(init): Server app initialized

---
+++ server/src/app.ts
@@ -0,0 +1,19 @@
+import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+const app = express();
+
+const port = process.env.PORT || 3000;
+
+const start = async () => {
+  try {
+    app.listen(port, () =>
+      console.log(`Server is listening on port ${port}...`)
+    );
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
