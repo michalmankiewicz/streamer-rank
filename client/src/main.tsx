e5628bc9f348dabac787fdd73eacda865de9b2c8
Michal Mankiewicz <michal.mankiewicz@itechart-group.com>
2023-07-02 22:50:47 +0200

    feat: Error boundary added

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import { CssBaseline } from '@mui/material';
+import ErrorBoundary from './components/ErrorBoundary/ErrorBoundary.tsx';
 
 const theme = createTheme({
   palette: {
@@ -28,9 +29,11 @@ const theme = createTheme({
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-      <App />
-    </ThemeProvider>
+    <ErrorBoundary>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <App />
+      </ThemeProvider>
+    </ErrorBoundary>
   </React.StrictMode>
 );
